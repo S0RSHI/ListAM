961b78d1fb15332684c30379919db50e83416ec3
Sorshi <damian.fuchler@gmail.com>
2022-06-25 18:49:08 +0200

    show some anime and manga on home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+       $anime_manga = DB::table('anime_manga')->get();
+        return view('home', compact('anime_manga'));
     }
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,61 @@
 @extends('layouts.app')
 
+<?php
+    $index_anime = 0;
+    $index_manga = 0;
+?>
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="v-grid">
-            <div class="v-grid__item">
-                <div class="v-tile">
-                    <img class="v-tile__img" src="/">
-                    <div class="v-tile__title">
-
-                    </div>
-                </div>
+        <section class="custom-section__01">
+            <h2 class="custom-title__01">
+                Recommended Anime
+            </h2>
+            <div class="grid-am">
+                @foreach ($anime_manga as $am_item)
+                    @if($am_item->is_anime == true)
+                        <?php
+                            $index_anime++;
+                        ?>
+                        <a class="grid-am__tile" src="/">
+                            <img class="grid-am__img" src="{{ $am_item->image_am }}">
+                            <div class="grid-am__overlay"></div>
+                            <div class="grid-am__title">
+                                {{ $am_item->title_am }}
+                            </div>
+                        </a>
+                        <?php
+                            if($index_anime == 5) break;
+                        ?>
+                    @endif
+                @endforeach
             </div>
-        </div>
+        </section>
+        <section class="custom-section__01">
+            <h2 class="custom-title__01">
+                Recommended Manga
+            </h2>
+            <div class="grid-am">
+                @foreach ($anime_manga as $am_item)
+                    @if($am_item->is_anime == false)
+                        <?php
+                            $index_manga++;
+                        ?>
+                        <a class="grid-am__tile" src="/">
+                            <img class="grid-am__img" src="{{ $am_item->image_am }}">
+                            <div class="grid-am__overlay"></div>
+                            <div class="grid-am__title">
+                                {{ $am_item->title_am }}
+                            </div>
+                        </a>
+                        <?php
+                            if($index_manga == 5) break;
+                        ?>
+                    @endif
+                @endforeach
+            </div>
+        </section>
     </div>
 </div>
 @endsection
