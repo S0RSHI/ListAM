2c61332333540343a47487dea364eb265fe814a7
Sorshi <damian.fuchler@gmail.com>
2022-06-25 20:12:33 +0200

    test?

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/test/{id}', [App\Http\Controllers\HomeController::class, 'test']);
+// Route::get('/test/{id}', [App\Http\Controllers\HomeController::class, 'test', id]);
